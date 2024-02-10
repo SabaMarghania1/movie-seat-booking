08237432eb3fb40b94264a7cce10f307dbad2906
Saba Marghania <marghanias@gmail.com>
2024-02-10 21:11:40 +0400

    finish:project, add localstorage and initial values with it

--- script.js
+++ script.js
@@ -4,21 +4,58 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 
 const movieSelect = document.getElementById("movie");
+populateUI();
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 
 function updateSelectedCount() {
-  const selectedSeeds = document.querySelectorAll(".row .seat.selected");
-  const selectedCount = selectedSeeds.length;
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  console.log(seatsIndex);
+
+  const selectedCount = selectedSeats.length;
   count.textContent = selectedCount;
   total.textContent = selectedCount * ticketPrice;
 }
 
+function populateUI() {
+  const selectedSeats = localStorage.getItem("selectedSeats");
+
+  if (selectedSeats.length >= 0 && !selectedSeats !== null) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+
+      const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+      if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+      }
+    });
+  }
+}
+
+movieSelect.addEventListener("change", e => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
 container.addEventListener("click", e => {
   if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
     e.target.classList.toggle("selected");
-
     updateSelectedCount();
   }
 });
+
+updateSelectedCount();
